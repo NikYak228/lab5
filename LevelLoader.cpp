3c4aeb3de375780fa85f678dac88ac1326caa130
nik <nikitayakimenko2356@gmail.com>
2026-01-03 23:23:19 +0300

    feat(build): integrate SDL2 library

--- LevelLoader.cpp
+++ LevelLoader.cpp
@@ -1,276 +1,273 @@
 #include "LevelLoader.h"
 #include <climits>
 #include <algorithm>
+#include <cmath>
 
-int LevelLoader::field_133 = 0;
-int LevelLoader::field_134 = 0;
-int LevelLoader::field_135 = 0;
-int LevelLoader::field_136 = 0;
+// Статические переменные
+int LevelLoader::visibleStart = 0;
+int LevelLoader::visibleEnd = 0;
+int LevelLoader::visibleStartX = 0;
+int LevelLoader::visibleEndX = 0;
 
 bool LevelLoader::isEnabledPerspective = true;
 bool LevelLoader::isEnabledShadows = true;
 
 LevelLoader::LevelLoader() {
+    // Предварительный расчет квадратов радиусов колес для оптимизации
     for (int i = 0; i < 3; ++i) {
-        field_123[i] = (int)((int64_t)((GamePhysics::const175_1_half[i] + 19660) >> 1) * (int64_t)((GamePhysics::const175_1_half[i] + 19660) >> 1) >> 16);
-        field_124[i] = (int)((int64_t)((GamePhysics::const175_1_half[i] - 19660) >> 1) * (int64_t)((GamePhysics::const175_1_half[i] - 19660) >> 1) >> 16);
+        wheelRadiusSq[i] = (int)((int64_t)((GamePhysics::const175_1_half[i] + 19660) >> 1) * (int64_t)((GamePhysics::const175_1_half[i] + 19660) >> 1) >> 16);
+        wheelInnerRadiusSq[i] = (int)((int64_t)((GamePhysics::const175_1_half[i] - 19660) >> 1) * (int64_t)((GamePhysics::const175_1_half[i] - 19660) >> 1) >> 16);
     }
 }
 
 LevelLoader::~LevelLoader() {
     delete gameLevel;
 }
+
 void LevelLoader::loadHardcodedLevel() {
     if (gameLevel == nullptr) {
         gameLevel = new GameLevel();
     }
     gameLevel->init();
-    gameLevel->setStartFinish(0, 100, 12000, 100);
+    
+    const int groundY = 220;
 
-    // 1) Стартовая зона — ровная
-    gameLevel->addPointSimple(-600, 100);
-    gameLevel->addPointSimple( 400, 100);
+    // --- НАСТРОЙКИ ПЕТЛИ ---
+    const int loopCenterX = 2200;
+    const int loopRadius  = 350;                  // Радиус петли
+    const int loopCenterY = groundY + loopRadius; // Центр петли: земля внизу, Y растет вверх
+    const int loopSteps   = 72;                   // Количество сегментов
 
-    // 2) Лёгкий подъём-спуск
-    gameLevel->addPointSimple( 600, 140);
-    gameLevel->addPointSimple( 800, 100);
+    // Старт/финиш
+    gameLevel->setStartFinish(0, groundY, loopCenterX + loopRadius + 1500, groundY);
 
-    // 3) Узкая яма
-    gameLevel->addPointSimple(1000,  60);
-    gameLevel->addPointSimple(1200,  60);
+    // 1. Разгонная прямая
+    gameLevel->addPointSimple(-400, groundY);
+    gameLevel->addPointSimple(   0, groundY);
+    gameLevel->addPointSimple( 800, groundY);
+    
+    // Плавный подход к петле
+    gameLevel->addPointSimple(loopCenterX - 300, groundY);
 
-    // 4) Платформа-разгон
-    gameLevel->addPointSimple(1500,  60);
+    // 2. Вход в петлю (нижняя точка)
+    gameLevel->addPointSimple(loopCenterX, groundY);
 
-    // 5) Большой трамплин в начале
-    gameLevel->addPointSimple(1600, 120);
-    gameLevel->addPointSimple(1800, 180);
-    gameLevel->addPointSimple(2000, 240);
-    gameLevel->addPointSimple(2200, 300); // «кикер»
+    // 3. Генерация самой петли
+    // Начинаем снизу (-PI/2) и идем против часовой стрелки, чтобы плавно выйти наверх
+    for (int step = 1; step <= loopSteps; ++step) {
+        double angleRad = -M_PI_2 + static_cast<double>(step) / loopSteps * 2.0 * M_PI;
+        
+        int x = loopCenterX + static_cast<int>(loopRadius * cos(angleRad));
+        int y = loopCenterY + static_cast<int>(loopRadius * sin(angleRad));
+        gameLevel->addPointSimple(x, y);
+    }
 
-    // 6) Зона приземления
-    gameLevel->addPointSimple(2400, 260);
-    gameLevel->addPointSimple(2600, 180);
-    gameLevel->addPointSimple(2800, 120);
+    // 4. Выход из петли
+    gameLevel->addPointSimple(loopCenterX + loopRadius + 1500, groundY); // Финишная прямая
 
-    // 7) Серия «волн»
-    gameLevel->addPointSimple(3100, 140);
-    gameLevel->addPointSimple(3300, 110);
-    gameLevel->addPointSimple(3500, 140);
-    gameLevel->addPointSimple(3700, 110);
-    gameLevel->addPointSimple(3900, 140);
-
-    // 8) Крутой подъём к финалу
-    gameLevel->addPointSimple(4200, 180);
-    gameLevel->addPointSimple(4400, 220);
-    gameLevel->addPointSimple(4600, 200);
-
-    // 9) Финишная прямая
-    gameLevel->addPointSimple(5000, 200);
-
-    // Передаём level-у информацию о триггерах старта/финиша
+    // Подготовка данных уровня (нормали и т.д.)
     prepareLevelData(gameLevel);
 }
 
-
-int LevelLoader::getFinishFlagX()
-{
+int LevelLoader::getFinishFlagX() {
     return gameLevel->pointPositions[gameLevel->finishFlagPoint][0] << 1;
 }
 
-int LevelLoader::getStartFlagX()
-{
+int LevelLoader::getStartFlagX() {
     return gameLevel->pointPositions[gameLevel->startFlagPoint][0] << 1;
 }
 
-int LevelLoader::getStartPosX()
-{
+int LevelLoader::getStartPosX() {
     return gameLevel->startPosX << 1;
 }
 
-int LevelLoader::getStartPosY()
-{
+int LevelLoader::getStartPosY() {
     return gameLevel->startPosY << 1;
 }
 
-int LevelLoader::getProgressAt(int var1)
-{
-    return gameLevel->computeProgress(var1 >> 1);
+int LevelLoader::getProgressAt(int posXF16) {
+    return gameLevel->computeProgress(posXF16 >> 1);
 }
 
-void LevelLoader::prepareLevelData(GameLevel* gameLevel)
-{
-    field_131 = INT_MIN;
+void LevelLoader::prepareLevelData(GameLevel* gameLevel) {
+    maxTrackX = INT_MIN;
     this->gameLevel = gameLevel;
-    int var2 = gameLevel->pointsCount;
-    if (field_121.empty() || field_132 < var2) {
-        field_132 = var2 < 100 ? 100 : var2;
-        field_121.assign(field_132, std::vector<int>(2));
+    int pointCount = gameLevel->pointsCount;
+    if (segmentNormals.empty() || segmentCapacity < pointCount) {
+        segmentCapacity = pointCount < 100 ? 100 : pointCount;
+        segmentNormals.assign(segmentCapacity, std::vector<int>(2));
     }
 
-    field_133 = 0;
-    field_134 = 0;
-    field_135 = gameLevel->pointPositions[field_133][0];
-    field_136 = gameLevel->pointPositions[field_134][0];
+    visibleStart = 0;
+    visibleEnd = 0;
+    visibleStartX = gameLevel->pointPositions[visibleStart][0];
+    visibleEndX = gameLevel->pointPositions[visibleEnd][0];
 
-    for (int var3 = 0; var3 < var2; ++var3) {
-        int var4 = gameLevel->pointPositions[(var3 + 1) % var2][0] - gameLevel->pointPositions[var3][0];
-        int var5 = gameLevel->pointPositions[(var3 + 1) % var2][1] - gameLevel->pointPositions[var3][1];
-        if (var3 != 0 && var3 != var2 - 1) {
-            field_131 = field_131 < gameLevel->pointPositions[var3][0] ? gameLevel->pointPositions[var3][0] : field_131;
+    for (int i = 0; i < pointCount - 1; ++i) {
+        // Расчет вектора сегмента (без замыкания последней точки на первую)
+        int dx = gameLevel->pointPositions[i + 1][0] - gameLevel->pointPositions[i][0];
+        int dy = gameLevel->pointPositions[i + 1][1] - gameLevel->pointPositions[i][1];
+        
+        if (i != 0 && i != pointCount - 1) {
+            maxTrackX = std::max(maxTrackX, gameLevel->pointPositions[i][0]);
         }
 
-        int var6 = -var5;
-        int var8 = GamePhysics::getSmthLikeMaxAbs(var6, var4);
-        field_121[var3][0] = (int)(((int64_t)var6 << 32) / (int64_t)var8 >> 16);
-        field_121[var3][1] = (int)(((int64_t)var4 << 32) / (int64_t)var8 >> 16);
-        if (gameLevel->startFlagPoint == 0 && gameLevel->pointPositions[var3][0] > gameLevel->startPosX) {
-            gameLevel->startFlagPoint = var3 + 1;
+        // Расчет нормали
+        int normalX = -dy;
+        int lengthF16 = GamePhysics::calcVectorLengthF16(normalX, dx);
+        
+        if (lengthF16 == 0) {
+            segmentNormals[i][0] = 0;
+            segmentNormals[i][1] = 0;
+            continue;
         }
-
-        if (gameLevel->finishFlagPoint == 0 && gameLevel->pointPositions[var3][0] > gameLevel->finishPosX) {
-            gameLevel->finishFlagPoint = var3;
-        }
+        
+        // Нормализация вектора
+        segmentNormals[i][0] = (int)(((int64_t)normalX << 32) / (int64_t)lengthF16 >> 16);
+        segmentNormals[i][1] = (int)(((int64_t)dx << 32) / (int64_t)lengthF16 >> 16);
     }
+    
+    // Явно фиксируем точки старта/финиша
+    gameLevel->startFlagPoint = 0;
+    gameLevel->finishFlagPoint = pointCount - 1;
 
-    field_133 = 0;
-    field_134 = 0;
-    field_135 = 0;
-    field_136 = 0;
+    // Сброс видимого диапазона
+    visibleStart = 0;
+    visibleEnd = 0;
+    visibleStartX = 0;
+    visibleEndX = 0;
 }
 
-void LevelLoader::setMinMaxX(int minX, int maxX)
-{
+void LevelLoader::setMinMaxX(int minX, int maxX) {
     gameLevel->setMinMaxX(minX, maxX);
 }
 
-void LevelLoader::renderTrackNearestLine(GameCanvas* canvas)
-{
-    canvas->setColor(0, 255, 0);
+void LevelLoader::renderTrackNearestLine(GameCanvas* canvas) {
+    // Трек рисуется как толстая линия (см. GameLevel::renderTrackNearestGreenLine)
     gameLevel->renderTrackNearestGreenLine(canvas);
 }
 
-void LevelLoader::updateVisibleRange(int var1, int var2, int var3)
-{
-    gameLevel->setSegmentRangeExact((var1 + 98304) >> 1, (var2 - 98304) >> 1, var3 >> 1);
-    var2 >>= 1;
-    var1 >>= 1;
-    field_134 = field_134 < gameLevel->pointsCount - 1 ? field_134 : gameLevel->pointsCount - 1;
-    field_133 = field_133 < 0 ? 0 : field_133;
-    if (var2 > field_136) {
-        while (field_134 < gameLevel->pointsCount - 1 && var2 > gameLevel->pointPositions[++field_134][0]) {
-        }
-    } else if (var1 < field_135) {
-        while (field_133 > 0 && var1 < gameLevel->pointPositions[--field_133][0]) {
-        }
-    } else {
-        while (field_133 < gameLevel->pointsCount && var1 > gameLevel->pointPositions[++field_133][0]) {
-        }
-
-        if (field_133 > 0) {
-            --field_133;
-        }
-
-        while (field_134 > 0 && var2 < gameLevel->pointPositions[--field_134][0]) {
-        }
-
-        field_134 = field_134 + 1 < gameLevel->pointsCount - 1 ? field_134 + 1 : gameLevel->pointsCount - 1;
-    }
-
-    field_135 = gameLevel->pointPositions[field_133][0];
-    field_136 = gameLevel->pointPositions[field_134][0];
+void LevelLoader::updateVisibleRange(int minXF16, int maxXF16, int cameraYF16) {
+    // Передаем точные координаты камере
+    gameLevel->setSegmentRangeExact((minXF16 + 98304) >> 1, (maxXF16 - 98304) >> 1, cameraYF16 >> 1);
+    
+    // ВАЖНО: Для корректной работы петли мы должны проверять ВСЕ сегменты уровня.
+    // Стандартная оптимизация (отсечение по X) сломает физику на вертикальных участках и потолке.
+    visibleStart = 0;
+    visibleEnd = gameLevel->pointsCount - 1;
+    
+    visibleStartX = minXF16 >> 1;
+    visibleEndX = maxXF16 >> 1;
 }
 
-int LevelLoader::detectCollision(TimerOrMotoPartOrMenuElem* var1, int var2)
-{
-    int var16 = 0;
-    int8_t var17 = 2;
-    int var18 = var1->xF16 >> 1;
-    int var19 = var1->yF16 >> 1;
+int LevelLoader::detectCollision(MotoComponent* wheel, int wheelIndex) {
+    int collisionsCount = 0;
+    int8_t collisionType = 2; // 2 = нет коллизии
+    int wheelX = wheel->xF16 >> 1;
+    int wheelY = wheel->yF16 >> 1;
+    
     if (isEnabledPerspective) {
-        var19 -= 65536;
+        wheelY -= 65536;
     }
 
-    int var20 = 0, var21 = 0;
+    int normalSumX = 0, normalSumY = 0;
 
-    for (int var22 = field_133; var22 < field_134; ++var22) {
-        int var4 = gameLevel->pointPositions[var22][0];
-        int var5 = gameLevel->pointPositions[var22][1];
-        int var6 = gameLevel->pointPositions[var22 + 1][0];
-        int var7;
-        if ((var7 = gameLevel->pointPositions[var22 + 1][1]) < var5) {
-            ;
-        }
+    for (int segmentIdx = visibleStart; segmentIdx < visibleEnd; ++segmentIdx) {
+        int x1 = gameLevel->pointPositions[segmentIdx][0];
+        int y1 = gameLevel->pointPositions[segmentIdx][1];
+        int x2 = gameLevel->pointPositions[segmentIdx + 1][0];
+        int y2 = gameLevel->pointPositions[segmentIdx + 1][1];
 
-        if (var18 - field_123[var2] <= var6 && var18 + field_123[var2] >= var4) {
-            int var8 = var4 - var6;
-            int var9 = var5 - var7;
-            int var10 = (int)((int64_t)var8 * (int64_t)var8 >> 16) + (int)((int64_t)var9 * (int64_t)var9 >> 16);
-            int var11 = (int)((int64_t)(var18 - var4) * (int64_t)(-var8) >> 16) + (int)((int64_t)(var19 - var5) * (int64_t)(-var9) >> 16);
-            int var12;
-            if ((var10 < 0 ? -var10 : var10) >= 3) {
-                var12 = (int)(((int64_t)var11 << 32) / (int64_t)var10 >> 16);
+        // --- ИСПРАВЛЕНИЕ ФИЗИКИ ПЕТЛИ ---
+        // Используем std::min и std::max для определения границ сегмента.
+        // Это позволяет корректно обрабатывать линии, идущие справа налево (потолок петли).
+        int segMinX = std::min(x1, x2);
+        int segMaxX = std::max(x1, x2);
+        int segMinY = std::min(y1, y2);
+        int segMaxY = std::max(y1, y2);
+        
+        int r = wheelRadiusSq[wheelIndex]; // Здесь это используется как радиус bounding box-а
+
+        // Проверка AABB (Axis-Aligned Bounding Box)
+        if (wheelX + r >= segMinX && wheelX - r <= segMaxX && 
+            wheelY + r >= segMinY && wheelY - r <= segMaxY) 
+        {
+            int segDX = x1 - x2;
+            int segDY = y1 - y2;
+            
+            // Квадрат длины сегмента
+            int segLenSq = (int)((int64_t)segDX * (int64_t)segDX >> 16) + (int)((int64_t)segDY * (int64_t)segDY >> 16);
+            
+            // Проекция центра колеса на прямую сегмента
+            int proj = (int)((int64_t)(wheelX - x1) * (int64_t)(-segDX) >> 16) + (int)((int64_t)(wheelY - y1) * (int64_t)(-segDY) >> 16);
+            
+            int tF16;
+            if ((segLenSq < 0 ? -segLenSq : segLenSq) >= 3) {
+                tF16 = (int)(((int64_t)proj << 32) / (int64_t)segLenSq >> 16);
             } else {
-                var12 = (var11 > 0 ? 1 : -1) * (var10 > 0 ? 1 : -1) * INT_MAX;
+                tF16 = (proj > 0 ? 1 : -1) * (segLenSq > 0 ? 1 : -1) * INT_MAX;
             }
 
-            if (var12 < 0) {
-                var12 = 0;
-            }
+            // Ограничиваем tF16 диапазоном [0, 65536] (отрезок от x1 до x2)
+            if (tF16 < 0) tF16 = 0;
+            if (tF16 > 65536) tF16 = 65536;
 
-            if (var12 > 65536) {
-                var12 = 65536;
-            }
-
-            int var13 = var4 + (int)((int64_t)var12 * (int64_t)(-var8) >> 16);
-            int var14 = var5 + (int)((int64_t)var12 * (int64_t)(-var9) >> 16);
-            var8 = var18 - var13;
-            var9 = var19 - var14;
-            int8_t var3;
-            int64_t var23;
-            if ((var23 = ((int64_t)var8 * (int64_t)var8 >> 16) + ((int64_t)var9 * (int64_t)var9 >> 16)) < (int64_t)field_123[var2]) {
-                if (var23 >= (int64_t)field_124[var2]) {
-                    var3 = 1;
+            // Ближайшая точка на сегменте
+            int closestX = x1 + (int)((int64_t)tF16 * (int64_t)(-segDX) >> 16);
+            int closestY = y1 + (int)((int64_t)tF16 * (int64_t)(-segDY) >> 16);
+            
+            int diffX = wheelX - closestX;
+            int diffY = wheelY - closestY;
+            
+            int8_t contactState;
+            int64_t distSq = ((int64_t)diffX * (int64_t)diffX >> 16) + ((int64_t)diffY * (int64_t)diffY >> 16);
+            
+            // Проверяем расстояние до сегмента
+            if (distSq < (int64_t)wheelRadiusSq[wheelIndex]) {
+                if (distSq >= (int64_t)wheelInnerRadiusSq[wheelIndex]) {
+                    contactState = 1; // Касание (мягкое)
                 } else {
-                    var3 = 0;
+                    contactState = 0; // Глубокое проникновение (жесткая коллизия)
                 }
             } else {
-                var3 = 2;
+                contactState = 2; // Нет контакта
             }
 
-            if (var3 == 0 && (int)((int64_t)field_121[var22][0] * (int64_t)var1->field_382 >> 16) + (int)((int64_t)field_121[var22][1] * (int64_t)var1->field_383 >> 16) < 0) {
-                field_137 = field_121[var22][0];
-                field_138 = field_121[var22][1];
+            // Расчет скалярного произведения скорости и нормали (движемся ли мы К стене)
+            int64_t dotProd = (int64_t)segmentNormals[segmentIdx][0] * (int64_t)wheel->velX + 
+                              (int64_t)segmentNormals[segmentIdx][1] * (int64_t)wheel->velY;
+
+            // Обработка жесткого удара
+            if (contactState == 0 && (dotProd >> 16) < 0) {
+                collisionNormalX = segmentNormals[segmentIdx][0];
+                collisionNormalY = segmentNormals[segmentIdx][1];
                 return 0;
             }
 
-            if (var3 == 1 && (int)((int64_t)field_121[var22][0] * (int64_t)var1->field_382 >> 16) + (int)((int64_t)field_121[var22][1] * (int64_t)var1->field_383 >> 16) < 0) {
-                ++var16;
-                var17 = 1;
-                if (var16 == 1) {
-                    var20 = field_121[var22][0];
-                    var21 = field_121[var22][1];
+            // Обработка мягкого касания
+            if (contactState == 1 && (dotProd >> 16) < 0) {
+                ++collisionsCount;
+                collisionType = 1;
+                if (collisionsCount == 1) {
+                    normalSumX = segmentNormals[segmentIdx][0];
+                    normalSumY = segmentNormals[segmentIdx][1];
                 } else {
-                    var20 += field_121[var22][0];
-                    var21 += field_121[var22][1];
+                    normalSumX += segmentNormals[segmentIdx][0];
+                    normalSumY += segmentNormals[segmentIdx][1];
                 }
             }
         }
     }
 
-    if (var17 == 1) {
-        if ((int)((int64_t)var20 * (int64_t)var1->field_382 >> 16) + (int)((int64_t)var21 * (int64_t)var1->field_383 >> 16) >= 0) {
+    // Если было касание с несколькими поверхностями (углы), усредняем нормаль
+    if (collisionType == 1) {
+        if ((int)((int64_t)normalSumX * (int64_t)wheel->velX >> 16) + (int)((int64_t)normalSumY * (int64_t)wheel->velY >> 16) >= 0) {
             return 2;
         }
 
-        field_137 = var20;
-        field_138 = var21;
+        collisionNormalX = normalSumX;
+        collisionNormalY = normalSumY;
     }
 
-    return var17;
-}
-
-// Метод renderLevel3D сделайте пустым:
-void LevelLoader::renderLevel3D(GameCanvas* gameCanvas, int xF16, int yF16) {
-    // Пусто, так как 3D-режим отключен
+    return collisionType;
 }
